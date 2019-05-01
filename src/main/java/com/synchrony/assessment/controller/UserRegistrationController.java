ffea295ba0968779c561941014bafb9f63e7ace1
vkrishnainduri <vkrishnainduri@gmail.com>
2019-05-01 12:48:48 +0530

    UserName and Password Authentication changes

---
+++ src/main/java/com/synchrony/assessment/controller/UserRegistrationController.java
@@ -0,0 +1,26 @@
+package com.synchrony.assessment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.synchrony.assessment.beans.UserInformation;
+import com.synchrony.assessment.service.UserInformationService;
+
+@Controller
+public class UserRegistrationController {
+
+	@Autowired
+	private UserInformationService userInformationService;
+
+	@RequestMapping(name = "/user/register",method = RequestMethod.POST)
+	@ResponseBody
+	public void registerUser(@RequestBody UserInformation userInformation) {
+		userInformationService.registerUser(userInformation);
+	}
+
+	
+}
--- src/main/java/com/synchrony/assessment/repository/MyUserInformationRepository.java
+++ src/main/java/com/synchrony/assessment/repository/MyUserInformationRepository.java
@@ -1,11 +1,14 @@
 package com.synchrony.assessment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.synchrony.assessment.beans.UserInformation;
 
 @Repository
 public interface MyUserInformationRepository extends JpaRepository<UserInformation, Long> {
-	public UserInformation findByUserName(String userName);
+	@Query(name = "SELECT * FROM USER_DETAILS WHERE USER_NAME = :userName")
+	public UserInformation findByUserName(@Param("userName") String userName);
 }
